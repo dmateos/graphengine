2df620d24174f5e726d0de238bd435a1d889a216
Daniel Mateos <daniel@mateos.cc>
2023-02-11 00:11:29 +1100

    graph code + fix relationship

--- graph/models.py
+++ graph/models.py
@@ -4,14 +4,20 @@ GRAPHTYPE_LINE = "line"
 GRAPHTYPE_BAR = "bar"
 
 
-class GraphPoint(models.Model):
-    data = models.CharField(max_length=32)
-
-
 class Graph(models.Model):
-    points = models.ForeignKey(GraphPoint, on_delete=models.CASCADE)
     name = models.CharField(max_length=32)
     type = models.CharField(max_length=16, default=GRAPHTYPE_LINE, choices=(
         (GRAPHTYPE_LINE, "line"),
         (GRAPHTYPE_BAR, "bar"),
     ))
+
+    def get_points(self):
+        return [1, 2, 3, 4, 5]*4
+
+    def get_labels(self):
+        return [1, 2, 3, 4, 5]*4
+
+
+class GraphPoint(models.Model):
+    data = models.CharField(max_length=32)
+    graph = models.ForeignKey(Graph, on_delete=models.CASCADE, null=False)
