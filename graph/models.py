1a7108b3cf1f4b9c49cb43ed238700ba62f7c81d
Daniel Mateos <daniel@mateos.cc>
2023-02-10 22:55:42 +1100

    support diff graph types

--- graph/models.py
+++ graph/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+GRAPHTYPE_LINE = "line"
+GRAPHTYPE_BAR = "bar"
+
 
 class GraphPoint(models.Model):
     data = models.CharField(max_length=32)
@@ -8,3 +11,7 @@ class GraphPoint(models.Model):
 class Graph(models.Model):
     points = models.ForeignKey(GraphPoint, on_delete=models.CASCADE)
     name = models.CharField(max_length=32)
+    type = models.CharField(max_length=16, default=GRAPHTYPE_LINE, choices=(
+        (GRAPHTYPE_LINE, "line"),
+        (GRAPHTYPE_BAR, "bar"),
+    ))
