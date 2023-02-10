3664ee57652ceb410a6efca7cb6978e59662f192
Daniel Mateos <daniel@mateos.cc>
2023-02-11 00:45:07 +1100

    label field + join to details page, and stricter fields

--- graph/models.py
+++ graph/models.py
@@ -5,8 +5,8 @@ GRAPHTYPE_BAR = "bar"
 
 
 class Graph(models.Model):
-    name = models.CharField(max_length=32)
-    type = models.CharField(max_length=16, default=GRAPHTYPE_LINE, choices=(
+    name = models.CharField(max_length=32, null=False)
+    type = models.CharField(max_length=16, null=False, default=GRAPHTYPE_LINE, choices=(
         (GRAPHTYPE_LINE, "line"),
         (GRAPHTYPE_BAR, "bar"),
     ))
@@ -16,9 +16,11 @@ class Graph(models.Model):
         return data
 
     def get_labels(self):
-        return [1, 2, 3]
+        label = [float(n.label) for n in self.graphpoint_set.all()]
+        return label
 
 
 class GraphPoint(models.Model):
-    data = models.CharField(max_length=32)
+    data = models.CharField(max_length=32, null=False)
+    label = models.CharField(max_length=8, default=0, null=False)
     graph = models.ForeignKey(Graph, on_delete=models.CASCADE, null=False)
