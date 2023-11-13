f1a45cb59b32b7da750e725d75f8cab690006b2c
Daniel Mateos <daniel@mateos.cc>
2023-11-13 11:00:57 +1100

    theme tweaks

--- graphengine/urls.py
+++ graphengine/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('graphs/', include("graph.urls")),
     path('l4mbda/', include("l4mbda.urls")),
-    #path('calculus/', include("calculus.urls")),
+    path('calculus/', include("calculus.urls")),
 ]
