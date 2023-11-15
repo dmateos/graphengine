46c8ddb442b3c8acaa9a300e489fb6f33821dcfe
Daniel Mateos <daniel@mateos.cc>
2023-11-15 16:10:14 +1100

    test cases

--- calculus/tests.py
+++ calculus/tests.py
@@ -13,3 +13,33 @@ class TestInterenceModelRun(django.test.TestCase):
 
         model.run_model("test-input")
         self.assertEqual(model.output, "test-input test-meta")
+
+
+class TestInferenceView(django.test.TestCase):
+    def test_get(self):
+        model = InferenceModel.objects.create(
+            name="test",
+            model_name="test",
+            metadata="test-meta",
+            output="",
+        )
+
+        response = self.client.get(f"/calculus/imodels/{model.pk}")
+        self.assertEqual(response.status_code, 200)
+
+    def test_post(self):
+        model = InferenceModel.objects.create(
+            name="test",
+            model_name="test",
+            metadata="test-meta",
+            output="",
+        )
+
+        response = self.client.post(f"/calculus/imodels/{model.pk}", {
+            "input": "test-input",
+        })
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, f"/calculus/imodels/{model.pk}")
+        model.refresh_from_db()
+        self.assertEqual(model.output, "test-input test-meta")
