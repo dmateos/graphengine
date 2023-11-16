ccc4c30d56ff42e85a821f2d01edbb174dbb8c21
Daniel Mateos <daniel@mateos.cc>
2023-11-17 01:33:17 +1100

    tweaks

--- graph/test_api_push.sh
+++ graph/test_api_push.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-graph=3
+graph=1
 sequ=0
 
 while :
 do
-        curl -H "Authorization: Token fa1e62b4d81d85a5a23ef6c7523d41a693e8d385" -d "label=&graph=$graph&data=$(( ( RANDOM % 1000 )  + 1 ))&sequence=$sequ" -X POST http://127.0.0.1:8000/api/graphpoints/
+        curl -H "Authorization: Token e49d33d3db99d484e9d4a14a142d547db720903d" -d "label=&graph=$graph&data=$(( ( RANDOM % 1000 )  + 1 ))&sequence=$sequ" -X POST http://127.0.0.1:8080/graphs/api/graphpoints/
         ((sequ=sequ+1))
 done
