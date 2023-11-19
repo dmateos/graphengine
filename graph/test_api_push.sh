9cc7a7ded1b0047c632216376729ccb494d5bd1c
Daniel Mateos <daniel@mateos.cc>
2023-11-19 23:32:38 +1100

    refactors

--- graph/test_api_push.sh
+++ graph/test_api_push.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-graph=1
+graph=3
 
 while :
 do
         curl -H "Authorization: Token e49d33d3db99d484e9d4a14a142d547db720903d" -d "label=t&graph=$graph&data=$(( ( RANDOM % 1000 )  + 1 ))" -X POST http://localhost:8080/graphs/api/graphpoints/
         echo ""
-        #sleep 1
+        sleep 1
 done
