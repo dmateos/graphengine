11401c5cb9bd5294af135b12a98bb5dda449a47d
Daniel Mateos <daniel@mateos.cc>
2024-02-11 17:40:28 +1100

    start on waverunner app

---
+++ waverunner/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+
+class Participant(models.Model):
+    name = models.CharField(max_length=100)
+    colour = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Competition(models.Model):
+    name = models.CharField(max_length=100)
+    date_and_time = models.DateTimeField()
+    participants = models.ManyToManyField(Participant)
+
+    def __str__(self):
+        return self.name
+
+
+class Result(models.Model):
+    race = models.ForeignKey(Competition, on_delete=models.CASCADE)
+    racer = models.ForeignKey(Participant, on_delete=models.CASCADE)
+    position = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.race.name} - {self.racer.name} - {self.position}"
+
+
+class Pick(models.Model):
+    race = models.ForeignKey(Competition, on_delete=models.CASCADE)
+    racer = models.ForeignKey(Participant, on_delete=models.CASCADE)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
