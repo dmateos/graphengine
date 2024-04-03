ffcb76d589bb5182846e2f82597f4550a68d1d74
Daniel Mateos <daniel@mateos.cc>
2024-04-04 00:55:22 +1100

    add config

---
+++ env_setup_bash.sh
@@ -0,0 +1,7 @@
+export MYSQL_HOST="mysql0.mateos.lan"
+export MYSQL_NAME="graph"
+export MYSQL_USER="graph"
+export MYSQL_PASSWORD="P455w0rd123#"
+
+export REDIS_HOST="redis0.mateos.lan"
+export REDIS_PASSWORD=""
