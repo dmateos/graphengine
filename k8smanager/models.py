b52c86749d74bc3f8ddfe9d4d23a931f6423083c
Daniel Mateos <daniel@mateos.cc>
2024-04-05 20:49:06 +1100

    add k8s lib

--- k8smanager/models.py
+++ k8smanager/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+import kubernetes
 
-# Create your models here.
+
+class Cluster(models.Model):
+    name = models.CharField(max_length=255)
+    cluster_endpoint = models.URLField()
+
+    def poll_cluster(self):
+        config = kubernetes.client.Configuration()
+        config.host = self.cluster_endpoint
+
+        client = kubernetes.client.CoreV1Api(
+            kubernetes.client.ApiClient(config)
+        )
+
+        return client.list_pod_for_all_namespaces()
