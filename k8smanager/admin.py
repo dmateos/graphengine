fca5261b733f019c1e937e6cd03034b1898e5a1f
Daniel Mateos <daniel@mateos.cc>
2024-04-06 00:11:56 +1100

    admin.py update

--- k8smanager/admin.py
+++ k8smanager/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Cluster
+from .models import Cluster, Node, Pod
 
 admin.site.register(Cluster)
+admin.site.register(Node)
+admin.site.register(Pod)
