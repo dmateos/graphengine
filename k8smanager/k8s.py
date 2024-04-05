3274b1351422895cc151d0b621f204f2b314d85c
Daniel Mateos <daniel@mateos.cc>
2024-04-06 00:04:59 +1100

    start on other models

--- k8smanager/k8s.py
+++ k8smanager/k8s.py
@@ -16,3 +16,9 @@ def get_pods_for_namespace(client, namespace):
     pods = client.list_namespaced_pod(namespace)
     pod_list = [pod.metadata.name for pod in pods.items]
     return pod_list
+
+
+def get_nodes(client):
+    nodes = client.list_node()
+    node_list = [node.metadata.name for node in nodes.items]
+    return node_list
--- k8smanager/models.py
+++ k8smanager/models.py
@@ -23,3 +23,29 @@ class Cluster(models.Model):
             return k8s.get_pods_for_namespace(client, namespace)
         except MaxRetryError as e:
             return f"Error: {e}"
+
+    def get_nodes(self):
+        try:
+            client = k8s.get_client(self.cluster_endpoint)
+            return k8s.get_nodes(client)
+        except MaxRetryError as e:
+            return f"Error: {e}"
+
+
+class Node(models.Model):
+    name = models.CharField(max_length=255)
+    cluster = models.ForeignKey(Cluster, on_delete=models.CASCADE)
+    status = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Pod(models.Model):
+    name = models.CharField(max_length=255)
+    cluster = models.ForeignKey(Cluster, on_delete=models.CASCADE)
+    namespace = models.CharField(max_length=255)
+    status = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
