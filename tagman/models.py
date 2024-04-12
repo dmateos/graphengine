5d68e31677f58874892aebb7b8c68dc7d487cd4c
Daniel Mateos <daniel@mateos.cc>
2024-04-13 00:43:04 +1000

    more basic auth code etc

--- tagman/models.py
+++ tagman/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import boto3
+import azure.mgmt.compute
 
 
 class Schedule(models.Model):
@@ -11,6 +12,25 @@ class Schedule(models.Model):
         return self.name
 
 
+class AWSAccessDetails(models.Model):
+    access_key = models.CharField(max_length=255)
+    secret_key = models.CharField(max_length=255)
+    region = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.access_key} - {self.region}"
+
+
+class AzureAccessDetails(models.Model):
+    subscription_id = models.CharField(max_length=255)
+    client_id = models.CharField(max_length=255)
+    secret = models.CharField(max_length=255)
+    tenant = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.subscription_id
+
+
 class UniversalTag(models.Model):
     key = models.CharField(max_length=255)
     value = models.CharField(max_length=255)
@@ -19,8 +39,13 @@ class UniversalTag(models.Model):
     def __str__(self):
         return f"{self.key}: {self.value}"
 
-    def get_aws_vms_with_tag(self):
-        aws = boto3.client("ec2")
+    def get_aws_vms_with_tag(self, auth_model):
+        aws = boto3.client(
+            "ec2",
+            region_name=auth_model.region,
+            aws_access_key_id=auth_model.access_key,
+            aws_secret_access_key=auth_model.secret_key,
+        )
         instances = aws.describe_instances()
         vms = []
         for reservation in instances["Reservations"]:
@@ -29,5 +54,12 @@ class UniversalTag(models.Model):
                     if tag["Key"] == self.key and tag["Value"] == self.value:
                         vms.append(instance)
 
-    def get_azure_vms_with_tag(self):
-        pass
+    def get_azure_vms_with_tag(self, auth_model):
+        azure = azure.mgmt.compute.ComputeManagementClient(
+            azure.common.credentials.ServicePrincipalCredentials(
+                client_id=auth_model.client_id,
+                secret=auth_model.secret,
+                tenant=auth_model.tenant,
+            ),
+            auth_model.subscription_id,
+        )
