428ccdb86a6aadc367f845a70646e1e279baaf79
Daniel Mateos <daniel@mateos.cc>
2024-04-13 01:42:16 +1000

    basic tag lookup working

--- tagman/views.py
+++ tagman/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import TemplateView, ListView
-from .models import UniversalTag
+from .models import UniversalTag, AWSAccessDetails, AzureAccessDetails
 
 
 class IndexView(TemplateView):
@@ -16,6 +16,25 @@ class TagListView(ListView):
 class TagDetailView(View):
     def get(self, request, *args, **kwargs):
         tag = UniversalTag.objects.get(pk=kwargs["pk"])
+        aws_auth = AWSAccessDetails.objects.first()
+        azure_auth = AzureAccessDetails.objects.first()
+
+        if aws_auth:
+            aws_vms = tag.get_aws_vms_with_tag(aws_auth)
+        else:
+            aws_vms = []
+
+        if azure_auth:
+            azure_vms = tag.get_azure_vms_with_tag(azure_auth)
+        else:
+            azure_vms = []
+
         return render(
-            request, "tagman/tag_detail.html", {"tag": tag}
+            request,
+            "tagman/tag_detail.html",
+            {
+                "tag": tag,
+                "aws_vms": aws_vms,
+                "azure_vms": azure_vms,
+            }
         )
