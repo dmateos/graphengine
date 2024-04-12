7a2aa5581d3d0e336da83b1232018dee4df0b837
Daniel Mateos <daniel@mateos.cc>
2024-04-13 00:01:21 +1000

    start of model

--- tagman/models.py
+++ tagman/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Schedule(models.Model):
+    name = models.CharField(max_length=255)
+    schedule = models.CharField(max_length=255)
+    action = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class UniversalTag(models.Model):
+    key = models.CharField(max_length=255)
+    value = models.CharField(max_length=255)
+    schedule = models.ForeignKey(Schedule, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.key}: {self.value}"
