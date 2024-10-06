eea8b697a827dfc3b577dd1f266aebd814a38be3
Daniel Mateos <daniel@mateos.cc>
2024-10-06 22:35:05 +1100

    add vector field

--- vecman/models.py
+++ vecman/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from pgvector.django import VectorField, HnswIndex
 
-# Create your models here.
+
+class File(models.Model):
+    embedding = VectorField(
+        dimensions=768,
+        help_text="Vector embeddings",
+        null=True,
+        blank=True,
+    )
+
+    class Meta:
+        indexes = [
+            HnswIndex(
+                name="clip_l14_vectors_index",
+                fields=["embedding"],
+                m=16,
+                ef_construction=64,
+                opclasses=["vector_cosine_ops"],
+            )
+        ]
