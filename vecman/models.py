1a4f48a92d1e16bc3fdd92c4e724745cd946e6a9
Daniel Mateos <daniel@mateos.cc>
2024-10-08 17:00:39 +1100

    more model tweaks

--- vecman/models.py
+++ vecman/models.py
@@ -37,7 +37,6 @@ class File(models.Model):
         model_name = "sentence-transformers/all-MiniLM-L6-v2"
         tokenizer = AutoTokenizer.from_pretrained(model_name)
         model = AutoModel.from_pretrained(model_name)
-
         inputs = tokenizer(data, return_tensors="pt", padding=True, truncation=True)
 
         with torch.no_grad():
